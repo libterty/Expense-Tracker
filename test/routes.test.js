f28db74e0c3cd6a4354822e278bba67e648779ad
dreak603 <dreak603@gmail.com>
2019-09-16 01:37:31 +0800

    add 14 fail test as expected

---
+++ test/routes.test.js
@@ -0,0 +1,141 @@
+const chai = require('chai')
+const chaiHTTP = require('chai-http')
+chai.use(chaiHTTP)
+const should = chai.should()
+const expect = chai.expect
+
+if (!global.Promise) {
+  global.Promise = require('q')
+}
+
+describe('GET /trackers', () => {
+  it('should return succees with user login', done => {
+    chai
+      .request('http://localhost:3001')
+      .post('/users/login')
+      .type('form')
+      .send({
+        _method: 'post',
+        email: 'user1@example.com',
+        password: '12345678'
+      })
+      .end((err, res) => {
+        should.not.exist(err)
+        expect(res).to.redirectTo('http://localhost:3001/')
+        done()
+      })
+  })
+
+  it('should return 200 if the session keep success', done => {
+    const agent = chai.request.agent('http://localhost:3001')
+    agent
+      .post('/users/login')
+      .type('form')
+      .send({
+        _method: 'post',
+        email: 'user1@example.com',
+        password: '12345678'
+      })
+      .then(res1 => {
+        agent.get('/restaurants/5d7732fc11207c1fe8bc4a97').then(res2 => {
+          res2.should.have.status(200)
+          done()
+        })
+      })
+  })
+
+  it('should return 400 if the restaurant is not exist', done => {
+    const agent = chai.request.agent('http://localhost:3001')
+    agent
+      .post('/users/login')
+      .type('form')
+      .send({
+        _method: 'post',
+        email: 'user1@example.com',
+        password: '12345678'
+      })
+      .then(res1 => {
+        agent.get('/trackers/1231231231231231231').then(res2 => {
+          expect(res2.status).to.be.eql(400)
+          done()
+        })
+      })
+  })
+
+  it('should return 200 if the edit is success', done => {
+    const agent = chai.request.agent('http://localhost:3001')
+    agent
+      .post('/users/login')
+      .type('form')
+      .send({
+        _method: 'post',
+        email: 'user1@example.com',
+        password: '12345678'
+      })
+      .then(res1 => {
+        agent
+          .put('/trackers/5d7732fc11207c1fe8bc4a97/edit')
+          .type('form')
+          .send({
+            _method: 'put',
+            name: '修改測試支出1',
+            category: '修改支出類別',
+            date: '修改測試時間',
+            amount: '修改支出金額'
+          })
+          .then(res2 => {
+            expect(res2.status).to.be.eql(200)
+            done()
+          })
+      })
+  })
+
+  it('should return 200 if create is success', done => {
+    const agent = chai.request.agent('http://localhost:3001')
+    agent
+      .post('/users/login')
+      .type('form')
+      .send({
+        _method: 'post',
+        email: 'user1@example.com',
+        password: '12345678'
+      })
+      .then(res1 => {
+        agent
+          .post('/trackers/new')
+          .type('form')
+          .send({
+            _method: 'post',
+            name: '測試支出2',
+            category: '支出類別',
+            date: '測試時間',
+            amount: '支出金額'
+          })
+          .then(res2 => {
+            expect(res2.status).to.be.eql(200)
+            done()
+          })
+      })
+  })
+
+  // selecting a objectId in ur database each of time before you process testing, _id will never be the same
+  it('should return 200 if delete is success', done => {
+    const agent = chai.request.agent('http://localhost:3001')
+    agent
+      .post('/users/login')
+      .type('form')
+      .send({
+        _method: 'post',
+        email: 'user1@example.com',
+        password: '12345678'
+      })
+      .then(res1 => {
+        agent
+          .delete('/trackers/5d7e1d41f38328af0b8f3bde/delete?_method=DELETE')
+          .then(res2 => {
+            expect(res2.status).to.be.eql(200)
+            done()
+          })
+      })
+  })
+})
---
+++ test/userAuth.test.js
@@ -0,0 +1,152 @@
+const chai = require('chai')
+const chaiHTTP = require('chai-http')
+chai.use(chaiHTTP)
+const should = chai.should()
+const expect = chai.expect
+
+describe('GET /', () => {
+  it('should return login page if a user is not logged in', done => {
+    chai
+      .request('http://localhost:3001')
+      .get('/')
+      .end((err, res) => {
+        should.not.exist(err)
+        expect(res).to.redirectTo('http://localhost:3001/users/login')
+        done()
+      })
+  })
+
+  it('should return to restaurant page', done => {
+    chai
+      .request('http://localhost:3001')
+      .post('/users/login')
+      .type('form')
+      .send({
+        _method: 'post',
+        email: 'user1@example.com',
+        password: '12345678'
+      })
+      .end((err, res) => {
+        should.not.exist(err)
+        expect(res).to.redirectTo('http://localhost:3001/')
+        done()
+      })
+  })
+
+  it('should logout a user', done => {
+    chai
+      .request('http://localhost:3001')
+      .get('/users/logout')
+      .end((err, res) => {
+        should.not.exist(err)
+        res.redirects.length.should.eql(0)
+        res.status.should.eql(200)
+        res.text.should.be.eql('你已經成功登出')
+        done()
+      })
+  })
+
+  it('should return back to login after fail login', done => {
+    chai
+      .request('http://localhost:3001')
+      .post('/users/login')
+      .type('form')
+      .send({
+        _method: 'post',
+        email: 'user1@example.com',
+        password: '11111'
+      })
+      .end((err, res) => {
+        should.not.exist(err)
+        expect(res).to.redirectTo('http://localhost:3001/users/login')
+        done()
+      })
+  })
+})
+
+describe('POST /users/register', () => {
+  describe('Form success', () => {
+    it('should return to the login if user signup success', done => {
+      chai
+        .request('http://localhost:3001')
+        .post('/users/register')
+        .type('form')
+        .send({
+          _method: 'post',
+          name: 'lib1',
+          email: 'lib1@example.com',
+          password: '12345678',
+          password2: '12345678'
+        })
+        .end((err, res) => {
+          should.not.exist(err)
+          expect(res).to.redirectTo('http://localhost:3001/users/login')
+          done()
+        })
+    })
+  })
+})
+
+describe('POST /users/register', () => {
+  describe('From Error', () => {
+    it('should return back 200 if user already exists ', done => {
+      chai
+        .request('http://localhost:3001')
+        .post('/users/register')
+        .type('form')
+        .send({
+          _method: 'post',
+          name: 'lib1',
+          email: 'lib1@example.com',
+          password: '12345678',
+          password2: '12345678'
+        })
+        .end((err, res) => {
+          should.not.exist(err)
+          res.should.be.status(200)
+          res.should.be.an('object')
+          done()
+        })
+    })
+
+    it('should return back 400 if users password with wrong input', done => {
+      chai
+        .request('http://localhost:3001')
+        .post('/users/register')
+        .type('form')
+        .send({
+          _method: 'post',
+          name: 'lib1',
+          email: 'lib1@example.com',
+          password: '12345678',
+          password2: '12342341'
+        })
+        .end((err, res) => {
+          should.not.exist(err)
+          res.should.be.status(400)
+          res.error.text.should.be.eql('密碼輸入錯誤')
+          done()
+        })
+    })
+
+    it('should return back 400 if users input blank', done => {
+      chai
+        .request('http://localhost:3001')
+        .post('/users/register')
+        .type('form')
+        .send({
+          _method: 'post',
+          name: 'lib2',
+          email: '',
+          password: '',
+          password2: ''
+        })
+        .end((err, res) => {
+          should.not.exist(err)
+          res.should.be.status(400)
+          res.error.text.should.be.eql('信箱和密碼是必填')
+          done()
+        })
+    })
+  })
+})
